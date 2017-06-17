c2cbda372b6491ead251990ecc99b73b011191c1
tuan230878 <tuan230878@yahoo.com>
2017-06-17 13:48:06 +1000

    play around with EventEmitter

---
+++ eventemitter.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const EventEmitter = require('events').EventEmitter;
+const Fs = require('fs');
+
+class FindPattern extends EventEmitter {
+    constructor(regex) {
+        super();
+        this.regex = regex;
+        this.files = [];
+    }
+
+    addFile(file) {
+        this.files.push(file);
+        return this;
+    }
+
+    find() {
+        var _self = this;
+        this.files.forEach(function(file) {
+                                Fs.readFile(file, 'utf-8', function(err,data) {
+                                        if (err) { return _self.emit('error', err, file);}
+                                        _self.emit('fileread', file);
+                                        let matches;
+                                        if (matches = data.match(_self.regex)){
+                                            matches.forEach(function(match) {
+                                                _self.emit('match',file,match);
+                                            })
+                                        }
+                                });
+        });    
+        return this;
+    }
+}
+/*
+function findPattern(files, regex) {
+    let emitter = new EventEmitter();
+    files.forEach(function(file) {
+        Fs.readFile(file, 'utf-8', (err,data) => {
+            if (err) { return emitter.emit('error', err, file);}
+            emitter.emit('fileread', file);
+            let matches;
+            if (matches = data.match(regex)){
+                matches.forEach(function(match) {
+                    emitter.emit('match',file,match);
+                })
+            }
+        })
+    });
+    return emitter;
+}
+
+findPattern(['./files/fileA.txt', './files/fileB.json'],/hello \w* /g).on('fileread', (file) => console.log(`fileread ${file}`))
+                .on('match', (file,match) => console.log(`matched ${match} in file ${file}`))
+                .on('error', (err,file) => console.log(`error ${err.message} in reading file ${file}`));
+*/
+var obj = new FindPattern(/hello \w*/g);
+obj.addFile('./files/fileA.txt')
+    .addFile('./files/fileB.json')
+    .on('fileread', (file) => console.log(`fileread ${file}`))
+    .on('match', (file,match) => console.log(`matched ${match} in file ${file}`))
+    .on('error', (err,file) => console.log(`error ${err.message} in reading file ${file}`))
+    .find();
